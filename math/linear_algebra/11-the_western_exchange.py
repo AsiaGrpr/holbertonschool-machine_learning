d82db60f1d6a6b586a266f4b3624789d3fa2e211
AsiaGrpr <groupierre.asia@gmail.com>
2023-12-07 01:41:51 -0800

    Write a function that transposes matrix with numpy

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Task 11 - "The Western Exchange"
+objective: Transpose the matrix
+"""
+
+
+def np_transpose(matrix):
+    """
+    Function to transpose a matrix with numpy
+    """
+
+    return matrix.transpose()
