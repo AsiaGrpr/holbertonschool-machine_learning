07b2c99539d218a73a53bffbc63ca03968116dd2
AsiaGrpr <groupierre.asia@gmail.com>
2023-12-07 02:01:33 -0800

    Write a function that performs matrix multiplication with numpy

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+Task 14 - "Saddle Up"
+objective: Matrix multiplication
+"""
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    Function to performs matrix multiplication with numpy
+    """
+
+    return np.matmul(mat1, mat2)
