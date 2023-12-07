8beff92a470f1b046e2b350ba8372c4df870e6af
AsiaGrpr <groupierre.asia@gmail.com>
2023-12-07 01:16:33 -0800

    Write a function that performs matrix multiplication

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""
+Task 8 - "Ridin’ Bareback"
+objective: Matrix multiplication
+"""
+
+
+def mat_mul(mat1, mat2):
+    """
+    Function to performs matrix multiplication
+    """
+
+    if len(mat1[0]) != len(mat2):
+        return None
+
+    result = [[0 for a in range(len(mat2[0]))] for b in range(len(mat1))]
+
+    for i in range(len(mat1)):
+        for j in range(len(mat2[0])):
+            for k in range(len(mat1[0])):
+                result[i][j] += mat1[i][k] * mat2[k][j]
+    return result
