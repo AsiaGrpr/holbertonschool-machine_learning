be5d70fa5cacc93c012456c193baf47f6ec9b020
AsiaGrpr <groupierre.asia@gmail.com>
2023-12-07 01:11:55 -0800

    Write a function that concatenates two matrices along a specific axis

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+Task 7 - "Gettin’ Cozy"
+objective: Concatenate matrices
+"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """
+    Function to concatenates two matrices along a specific axis
+    """
+
+    if axis == 0 and len(mat1[0]) == len(mat2[0]):
+        return mat1 + mat2
+    elif axis != 0 and len(mat1) == len(mat2):
+        return [row1 + row2 for row1, row2 in zip(mat1, mat2)]
+    else:
+        return None
+    
