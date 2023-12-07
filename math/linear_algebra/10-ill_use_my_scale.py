45b142e3171e838732cccd884f60da662291cbcc
AsiaGrpr <groupierre.asia@gmail.com>
2023-12-07 01:37:00 -0800

    Write a function that calculates the shape of a matrix with numpy

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+Task 10 - "I’ll Use My Scale"
+objective: Calculate the shape of a matrix
+"""
+
+
+def np_shape(matrix):
+    """
+    Function to calculate the shape of a matrix with numpy
+    """
+
+    return matrix.shape
