a6d8964a6aa55e7e996f57713cc2dcd5e4131fa9
AsiaGrpr <groupierre.asia@gmail.com>
2023-12-19 00:17:01 -0800

    Instance methods def z_score and def x_value added

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -24,3 +24,17 @@ class Normal:
             self.mean = float(sum(data) / len(data))
             self.stddev = float(
                 (sum((x - self.mean)**2 for x in data) / len(data)) ** 0.5)
+
+    def z_score(self, x):
+        """
+        Function to calculate the z-score of a given x-value
+        """
+
+        return (x - self.mean) / self.stddev
+
+    def x_value(self, z):
+        """
+        Function to calculate the x-score given z-score
+        """
+
+        return self.mean + z * self.stddev
